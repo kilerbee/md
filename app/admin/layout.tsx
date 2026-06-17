f1aaea2fed57a5fda58ef541fb8ac4ff2d60023e
Maks Sidorenko <maksim.sidorenko@bnberry.com>
2026-06-17 11:14:04 +0200

    export on the admin index

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -31,7 +31,6 @@ export default async function AdminLayout({
           <Link href="/admin/artists">Artists</Link>
           <Link href="/admin/venues">Venues</Link>
           <Link href="/admin/import">Import</Link>
-          <Link href="/admin/export">Export</Link>
           <Link href="/">Public site</Link>
         </nav>
       </header>
