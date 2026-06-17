4cc01b814bd807809a88bcf87a47f14e8f236b1e
Maks Sidorenko <maksim.sidorenko@bnberry.com>
2026-06-17 11:32:40 +0200

    export fix

--- components/shared/CopyTextarea.tsx
+++ components/shared/CopyTextarea.tsx
@@ -2,8 +2,8 @@
 
 export function CopyTextarea({ children }: { children: React.ReactNode }) {
   return (
-    <pre
-      className="mt-4 h-[60vh] w-full overflow-auto border border-neutral-300 px-3 py-2 font-sans text-sm leading-relaxed"
+    <div
+      className="mt-4 h-[60vh] w-full overflow-auto border border-neutral-300 px-3 py-2 text-sm leading-relaxed"
       onClick={(e) => {
         const selection = window.getSelection();
         if (selection) {
@@ -15,6 +15,6 @@ export function CopyTextarea({ children }: { children: React.ReactNode }) {
       }}
     >
       {children}
-    </pre>
+    </div>
   );
 }
